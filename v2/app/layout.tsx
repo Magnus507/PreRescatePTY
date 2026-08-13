3f04e9aef73faa76e8346c2584bffb7a8a41c193
Gean Cusatti <highspeed1310@gmail.com>
2026-08-13 15:59:17 -0500

    v2: add root layout

---
+++ v2/app/layout.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+import "./globals.css";
+export default function RootLayout({children}:{children:React.ReactNode}){return React.createElement("html",{lang:"es"},React.createElement("body",null,children));}
