0ba88230090c4a96b813d272ed5e84d6a636e51e
Gean Cusatti <highspeed1310@gmail.com>
2026-08-13 15:59:27 -0500

    v2: add home page

---
+++ v2/app/page.tsx
@@ -0,0 +1,2 @@
+import React from "react";
+export default function Home(){return React.createElement("main",{className:"main"},React.createElement("h1",null,"PreRescatePTY 2.0"),React.createElement("p",null,"Nueva aplicación en construcción."));}
