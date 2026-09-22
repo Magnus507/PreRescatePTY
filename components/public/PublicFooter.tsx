53ddd7c1a7ceb4ac682abb56ae63369f165e282e
Gean Cusatti <highspeed1310@gmail.com>
2026-09-21 23:26:26 -0500

    Fix unused footer import after demo removal

--- components/public/PublicFooter.tsx
+++ components/public/PublicFooter.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { ArrowUpRight, Cookie, Heart, QrCode, ShieldCheck } from "lucide-react";
+import { Cookie, Heart, QrCode, ShieldCheck } from "lucide-react";
 
 const footerColumns = [
   {
